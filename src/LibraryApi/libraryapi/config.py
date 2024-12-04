bb2ea2ec7b118db8af2de59edf3c20fcf8e735cd
BrunoMoraix <143199125+BrunoMoraix@users.noreply.github.com>
2024-12-04 00:00:25 -0300

    Delete src/LibraryApi directory

--- src/LibraryApi/libraryapi/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'library.db')}"  # Banco SQLite
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Evita warnings desnecessários
